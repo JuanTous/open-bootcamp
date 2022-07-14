7775ebccdf6ab02eb665bc4d4429cecdc49ed365
Juan Tous <tousjuan01@gmail.com>
2022-07-14 12:31:32 -0500

    tema 2

---
+++ Curso Python/tema 2/lesson_1.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    hello_world = "Hola mundo!"
+    print(hello_world)
+    print(type(hello_world))
---
+++ Curso Python/tema 2/lesson_2.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    hello_world = "Hola mundo!"
+    hello_world = "Hello world!"
+    print(hello_world)
+    print(type(hello_world))
